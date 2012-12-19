e39e2a07b7973a770e27a612469ae4344e49fce3
Paco <paco@paco-acer-mint.(none)>
2012-12-19 18:39:44 +0100

    Fin eval. Manque toujours user task

--- webapps/EnseirbWebXMLWebapp/src/fr/enseirb/webxml/servlet/OwnershipServlet.java
+++ webapps/EnseirbWebXMLWebapp/src/fr/enseirb/webxml/servlet/OwnershipServlet.java
@@ -7,9 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.Properties;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.w3c.dom.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.*;
+import java.io.*;
+import javax.xml.transform.stream.*;
+
+import fr.enseirb.webxml.util.StringFormatUtil;
 import fr.enseirb.webxml.util.ServletToolkit;
 import fr.enseirb.webxml.data.xml.XMLMediator;
 import fr.enseirb.webxml.util.XMLToolkit;
@@ -18,14 +26,14 @@ import fr.enseirb.webxml.data.model.User;
 /**
  * Servlet implementation class AboutServlet
  */
-public class CRUDTaskServlet extends HttpServlet {
+public class OwnershipServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public CRUDTaskServlet() {
+	public OwnershipServlet() {
 		super();
 	}
 
@@ -33,67 +41,77 @@ public class CRUDTaskServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String sResponse;
-		Properties props = ServletToolkit.parseURLParams(request);
+		String sResponse = new String("coucou");
 		
-		if(request.getRequestURI().contains("task/create/url"))
+		Properties props;
+		
+		String postData;
+		
+		if(request.getRequestURI().contains("task/user/ownership/post/isValid") || request.getRequestURI().contains("user/task/ownership/post/isValid"))
 		{
+			postData = ServletToolkit.getPostData(request);
+			String ownershipXML = postData;
+			boolean success = XMLToolkit.isXMLValid(ownershipXML, "resources/xsd/ownership.xsd");
+			sResponse = XMLToolkit.createPostResult("coucou", success);
+		}
+		else if(request.getRequestURI().contains("task/user/ownership/post") || request.getRequestURI().contains("task/user/ownership/post"))
+		{
+			int taskId;
+			String userName;
 			
-			String taskXML;
-			if(props.containsKey("title"))
-			{
-				int id=-1;
-				boolean done=false;
-				if(props.containsKey("id"))
-					id = Integer.parseInt(props.getProperty("id"));
-				if(props.containsKey("done"))
-					done = Boolean.valueOf(props.getProperty("done"));
-					
-				taskXML="<task id=\""+ id + "\" title=\""+ props.getProperty("title") + "\" deadline=\""+ props.getProperty("deadline") + "\" priority=\""+ props.getProperty("priority") + "\" done=\"" + done + "\">";
-				taskXML=taskXML + "<description>"+props.getProperty("description")+" </description>";
-				taskXML=taskXML + "<asker>"+props.getProperty("asker")+"</asker> ";
-				taskXML=taskXML + "<owner>"+props.getProperty("owner")+"</owner> ";
-				taskXML=taskXML + "</task>";
-				
-				response.setHeader("Content-Type", "text/html");
-				if(XMLMediator.addOrModifyTask(taskXML))
-					sResponse = "Tache cr&eacute;&eacute;e.";
-				else
-					sResponse = "Erreur lors de la creation de la tache";
-			}
-			else	
-			{
-				sResponse = "Aucun argument";
-			}
-		}
-		else
-		{
-			String fluxXML="<noXML/>";
-			Map<String, String> xslParams = new HashMap<String, String>();
-			int id;
-			if(props.containsKey("id"))
-				id=Integer.parseInt((props.getProperty("id")));
-			else id=-1;
-			if(request.getRequestURI().contains("task/create"))
-			{
-				xslParams.put("pageState", "CREATE");
-				fluxXML=XMLMediator.createEmptyTaskXML();
-			}
-			else if(request.getRequestURI().contains("task/view"))
-			{
-				xslParams.put("pageState", "READONLY");
-				fluxXML=XMLMediator.getTask(id);
-			}
-			else if(request.getRequestURI().contains("task/modify"))
-			{
-				xslParams.put("pageState", "MODIFY");
-				fluxXML=XMLMediator.getTask(id);
-			}
-				
-			sResponse=XMLToolkit.transformXML(fluxXML, "/resources/xsl/common/crud_task.xsl", xslParams);
-		}
+			postData = ServletToolkit.getPostData(request);
+			Document doc = XMLToolkit.parseDocument(postData);
+			Element ownershipElt = doc.getDocumentElement();
+			
+			taskId = Integer.parseInt(ownershipElt.getAttribute("taskId"));
+			userName = ownershipElt.getAttribute("userName");
+			
+			String taskXML = XMLMediator.getTask(taskId);
+			doc = XMLToolkit.parseDocument(taskXML);
+			Element taskElt = doc.getDocumentElement();
+			
+			Element ownerElt = (Element) taskElt.getElementsByTagName("owner").item(0);
+			ownerElt.setTextContent(userName);
 		
+			
+			
+			if(XMLMediator.addOrModifyTask(taskElt))
+				sResponse = new String("Proprietaire modifie");
+			else
+				sResponse = new String("erreur");
+			
+		}
+		else if(request.getRequestURI().contains("user/ownership"))
+		{
+			props = ServletToolkit.parseURLParams(request);
+			//int taskId = Integer.parseInt(props.getProperty("taskId"));
+			String userName = props.getProperty("userName");
+			response.setHeader("Content-Type", "text/plain");
+			String tasksXML = XMLMediator.getTasks();
+			
+			List <String> idList = XMLToolkit.getXPathValues(tasksXML, "/tasks/task[owner='"+userName+"']/@id");
+			
+			sResponse = StringFormatUtil.sortAndFormat(idList);
+
+			
+		}
+		else if(request.getRequestURI().contains("task/ownership"))
+		{
+			props = ServletToolkit.parseURLParams(request);
+			int taskId = Integer.parseInt(props.getProperty("taskId"));
+			//String userName = props.getProperty("userName");
+			response.setHeader("Content-Type", "text/plain");
+			String tasksXML = XMLMediator.getTasks();
+			
+			List <String> idList = XMLToolkit.getXPathValues(tasksXML, "/tasks/task[@id='"+taskId+"']/owner/text()");
+			
+			sResponse = StringFormatUtil.sortAndFormat(idList);
+			
+			
+		}
+
 		ServletToolkit.writeResponse(response, sResponse);
+
 	}
 
 	@Override
